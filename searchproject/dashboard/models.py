b6b4d24101664ef18713626c9335ccf94bb111ed
Poollerd <poollerd9735@gmail.com>
2021-12-12 17:20:11 +0700

    finish  reeach

---
+++ searchproject/dashboard/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import aggregates
+
+# Create your models here.
+class Data(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.first_name
